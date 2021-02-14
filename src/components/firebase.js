d78d55cc051df2c414a0449a347719f283a7bb5d
John Paeldin <vongolajpaeldin@gmail.com>
2021-02-14 13:09:51 +0800

    Rebase config for firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firebase-database';
 import 'firebase/firebase-auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIOqVyDGBZ4wvFYTNzVp85ViEuv_V8G6E",
-    authDomain: "flcosmetics-34043.firebaseapp.com",
-    databaseURL: "https://flcosmetics-34043.firebaseio.com",
-    projectId: "flcosmetics-34043",
-    storageBucket: "flcosmetics-34043.appspot.com",
-    messagingSenderId: "435594154444",
-    appId: "1:435594154444:web:57cb221a650f40c60d0d69",
-    measurementId: "G-3B59G472F3"
+    apiKey: "AIzaSyDa8eJVeNBKDuvI3zR5hIFUuyDy3KqWUUo",
+    authDomain: "forthnite.firebaseapp.com",
+    databaseURL: "https://forthnite-default-rtdb.firebaseio.com",
+    projectId: "forthnite",
+    storageBucket: "forthnite.appspot.com",
+    messagingSenderId: "818259694637",
+    appId: "1:818259694637:web:6f4eaa0047e3e2a416d4a4",
+    measurementId: "G-X8QJ3Y6503"
   };
   // Initialize Firebase
  const fire = firebase.initializeApp(firebaseConfig);
